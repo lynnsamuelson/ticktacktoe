cc6f8e8485a8f87f51d367d7aaec99e025e76596
Dan-Hodges <dhdgs690@gmail.com>
2015-07-24 11:20:13 -0500

    Updated styling and added scoreboard

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
 var dataArray = [];
+var winner = "";
+var scoreX = 0;
+var scoreO = 0;
 function score () {
   if ((dataArray[1] === "X" && dataArray[2] === "X" && dataArray[3] === "X") ||
       (dataArray[1] === "X" && dataArray[4] === "X" && dataArray[7] === "X") ||
@@ -8,8 +11,11 @@ function score () {
       (dataArray[2] === "X" && dataArray[5] === "X" && dataArray[8] === "X") || 
       (dataArray[3] === "X" && dataArray[6] === "X" && dataArray[9] === "X") || 
       (dataArray[3] === "X" && dataArray[5] === "X" && dataArray[7] === "X")) {
-    $('#winner').text('X Wins!');
+    winner = "X Wins!";
+    $('#winner').text(winner);
     console.log("X Wins!");
+    scoreX += 1;
+    $('#scrX').text("X Score: " + scoreX);
   }
   if ((dataArray[1] === "O" && dataArray[2] === "O" && dataArray[3] === "O") ||
       (dataArray[1] === "O" && dataArray[4] === "O" && dataArray[7] === "O") ||
@@ -19,13 +25,17 @@ function score () {
       (dataArray[2] === "O" && dataArray[5] === "O" && dataArray[8] === "O") || 
       (dataArray[3] === "O" && dataArray[6] === "O" && dataArray[9] === "O") || 
       (dataArray[3] === "O" && dataArray[5] === "O" && dataArray[7] === "O")) {
-    $('#winner').text('O Wins!');
+    winner = "O Wins!";
+    $('#winner').text(winner);
     console.log("O Wins!");
+    scoreO += 1;
+    $('#scrO').text("O Score: " + scoreO);
   }
 };
-$('td').click(function() {
+$('td:not(#reset)').click(function() {
   $(this).text('X');
   $(this).addClass("X");
+  $('#winner').text("");
   dataArray [1] = $('#one').text();
   dataArray [2] = $('#two').text();;
   dataArray [3] = $('#three').text();;
@@ -37,10 +47,12 @@ $('td').click(function() {
   dataArray [9] = $('#nine').text();;
   console.log(dataArray);
   score();
+  
 });
-$('td').dblclick(function() {
+$('td:not(#reset)').dblclick(function() {
   $(this).text('O');
   $(this).attr('class', 'O');
+  $('#winner').text("");
   dataArray [1] = $('#one').text();
   dataArray [2] = $('#two').text();;
   dataArray [3] = $('#three').text();;
@@ -53,4 +65,10 @@ $('td').dblclick(function() {
   console.log(dataArray);
   score();
 });
-
+$('#reset').click(function() {
+  $('td:not(#reset)').text("");
+  dataArray.splice(0,dataArray.length);
+  $('#winner').text("")
+});
+$('#scrX').text("X Score: " + scoreX);
+$('#scrO').text("O Score: " + scoreO);
